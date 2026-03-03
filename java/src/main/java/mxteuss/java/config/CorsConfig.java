d1cbe2c6df0bd1dd947ca58fa78b566d8c5640b7
mxteuss <ovagabundant@gmail.com>
2026-03-03 13:17:20 -0300

    chore: cors permite todos origins

--- java/src/main/java/mxteuss/java/config/CorsConfig.java
+++ java/src/main/java/mxteuss/java/config/CorsConfig.java
@@ -10,10 +10,11 @@ public class CorsConfig     {
     @Bean
     public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
+            @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("https://abntify.vercel.app")
-                        .allowedMethods("POST", "OPTIONS")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
 
