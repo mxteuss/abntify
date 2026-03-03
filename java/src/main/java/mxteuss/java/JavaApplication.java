77da1883ebdef2cfc33da9b58a9cb1fb6c958fac
mxteuss <ovagabundant@gmail.com>
2026-03-03 15:35:01 -0300

    chore: alterar versão

--- java/src/main/java/mxteuss/java/JavaApplication.java
+++ java/src/main/java/mxteuss/java/JavaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(JavaApplication.class, args);
 	}
 
