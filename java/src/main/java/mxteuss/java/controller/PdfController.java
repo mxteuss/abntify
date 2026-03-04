db93f73ca2d0a05c62195dd9d85689446282b6b9
mxteuss <ovagabundant@gmail.com>
2026-03-04 14:48:50 -0300

    feat: adicionar endpoint para ver histórico

--- java/src/main/java/mxteuss/java/controller/PdfController.java
+++ java/src/main/java/mxteuss/java/controller/PdfController.java
@@ -8,6 +8,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @CrossOrigin(origins = "*")
 public class PdfController {
@@ -20,17 +24,32 @@ public class PdfController {
 
 
     @PostMapping("/gerar-pdf")
-    public ResponseEntity<byte[]> getPdf(@RequestBody PdfModel dados) {
+    public ResponseEntity<byte[]> fazerPdf(@RequestBody PdfModel dados) {
         try {
             byte[] pdf = pdfService.gerarPdfABNT(dados);
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_PDF);
             headers.setContentDispositionFormData("attachment", "abnt.pdf");
+
             return new ResponseEntity<>(pdf, headers, HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-}
+
+    @GetMapping("/historico")
+    public List<Map<String, Object>>  listarPdf(){
+        return pdfService.listPDF().stream().map(historico -> {
+            Map<String, Object> item = new HashMap<>();
+            item.put("id", historico.getId());
+            item.put("nome", historico.getNomeArquivo());
+            item.put("descricao", historico.getDescricao());
+            item.put("geradoEm", historico.getGeradoEm());
+
+            return item;
+        }).toList();
+        }
+    }
+
