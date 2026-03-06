66fe2e1d746436275b2c3c39472449c323151c51
mxteuss <ovagabundant@gmail.com>
2026-03-05 21:42:16 -0300

    feat: adicionar primary key

--- java/src/main/java/mxteuss/java/model/PdfModel.java
+++ java/src/main/java/mxteuss/java/model/PdfModel.java
@@ -1,12 +1,18 @@
 package mxteuss.java.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import java.util.UUID;
 
 @Data
+@Entity
 public class PdfModel {
 
-
+    @Id
+    @GeneratedValue(strategy =  GenerationType.UUID)
     private UUID id;
     private String curso;
     private String titulo;
