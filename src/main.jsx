88fb5a6ab042109ed904ab100ea8731e373937fb
mxteuss <ovagabundant@gmail.com>
2026-03-19 17:29:43 -0300

    chore: alterando nome de arquivo

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../index.css';
 import ABNTify from './index.jsx';
-import Historico from './Historico.jsx';
+import Historico from './historico.jsx';
 import '../historico.css';
 
 createRoot(document.getElementById('root')).render(
