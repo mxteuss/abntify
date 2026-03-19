b56996ee1fc14ebb3a73eb2f42b79364c7a73814
mxteuss <ovagabundant@gmail.com>
2026-03-19 17:15:19 -0300

    chore: adicionar historico ao main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import '../index.css';
 import ABNTify from './index.jsx';
+import Historico from './Historico.jsx';
+import '../historico.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ABNTify />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ABNTify />} />
+        <Route path="/historico" element={<Historico />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
