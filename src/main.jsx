7b56f14c4565726dbe6b48a347d0ca4ea129a9b3
mxteuss <ovagabundant@gmail.com>
2026-04-19 22:11:26 -0300

    chore: alterar local de import do css

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import '../index.css';
 import ABNTify from './index.jsx';
 import Historico from './historico.jsx';
+import './index.css';
 import '../historico.css';
 
 createRoot(document.getElementById('root')).render(
